e3d6b7a9a436edc3ade925de78e09036097a917c
Sayantan Das <32683338+SayantanD99@users.noreply.github.com>
2024-09-10 08:49:51 +0530

    Initial Draft

---
+++ src/main/java/org/example/parkinglot/models/Gate.java
@@ -0,0 +1,49 @@
+package org.example.parkinglot.models;
+
+public class Gate extends BaseModel {
+    private int gateNumber;
+    private Operator operator;
+    private GateType gateType;
+    private GateStatus gateStatus;
+    private ParkingLot parkingLot;
+
+    public int getGateNumber() {
+        return gateNumber;
+    }
+
+    public void setGateNumber(int gateNumber) {
+        this.gateNumber = gateNumber;
+    }
+
+    public Operator getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Operator operator) {
+        this.operator = operator;
+    }
+
+    public GateType getGateType() {
+        return gateType;
+    }
+
+    public void setGateType(GateType gateType) {
+        this.gateType = gateType;
+    }
+
+    public GateStatus getGateStatus() {
+        return gateStatus;
+    }
+
+    public void setGateStatus(GateStatus gateStatus) {
+        this.gateStatus = gateStatus;
+    }
+
+    public ParkingLot getParkingLot() {
+        return parkingLot;
+    }
+
+    public void setParkingLot(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+}
